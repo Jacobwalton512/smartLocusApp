29a76242ad3ddddfc7ad4b2babf6609c59e39c13
Jacob Walton <jacobwalton@Jacobs-iMac.local>
2022-02-08 11:42:31 -0600

    init commit

--- dataviz/src/App.js
+++ dataviz/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello John
+          Hello John and Jacob
         </a>
       </header>
     </div>
